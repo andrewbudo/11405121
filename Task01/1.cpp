fab62183497de789a24dade6cd2f3212c3d4283a
nozim747 <nozim@example.com>
2023-12-02 09:48:47 +0300

    task01

--- Task01/1.cpp
+++ Task01/1.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
-
-int main()
+#include <vector>
+int
+main ()
 {
-	
-	return 0;
+  int N, K;
+  std::cin >> N >> K;
+  std::vector < int >arr;
+  for (int i = 0; i < N; i++)
+    {
+      int value;
+      std::cin >> value;
+      arr.push_back (value);
+    }
+
+  int diff = 0;
+  int count = 0;
+  for (int i = 0; i < arr.size (); i++)
+    {
+      for (int j = 1; j < arr.size (); j++)
+	{
+	  diff = arr[j] - arr[i];
+	  if (diff == K)
+	    {
+	      count++;
+	    }
+	}
+    }
+  std::cout << count << std::endl;
+  return 0;
 }
