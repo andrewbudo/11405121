b4393ce274d09bcae50ef03c42c9e2fdf7e0ff37
stud <reginakeytt44@gmail.com>
2023-12-07 13:50:31 +0300

    1.cpp

---
+++ Task01/1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+  int N;
+  std::cin >> N;
+  if (N == 2) {
+    std::cout << "NO" << std::endl;
+    return 0;
+  }
+
+  if (N % 2 == 0) {
+    std::cout << "YES" << std::endl;
+    return 0;
+  }
+
+  std::cout << "NO" << std::endl;
+  return 0;
+}
